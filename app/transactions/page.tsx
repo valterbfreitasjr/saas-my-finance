6307249e2f40074b24be750e3384a5711eff6226
Valter B. Freitas Jr <valterbfreitasjr@gmail.com>
2024-11-08 17:24:26 -0300

    chore: fix amount on transaction table

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -1,5 +1,17 @@
-const TransactionPage = () => {
-  return <h1>Transaction Page</h1>;
+import { db } from "../_lib/prisma";
+
+const TransactionPage = async () => {
+  const transactions = await db.transaction.findMany({});
+
+  return (
+    <div>
+      {transactions.map((transaction) => (
+        <div key={transaction.id}>
+          {transaction.name} - {transaction.amount}
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default TransactionPage;
