0fcfff6237dc1113a16278aac0313ba95476d73c
Valter B. Freitas Jr <valterbfreitasjr@gmail.com>
2024-11-19 15:03:06 -0300

    feat: add transaction button component

--- app/_components/add-transaction-button.tsx
+++ app/_components/add-transaction-button.tsx
@@ -2,7 +2,6 @@
 
 import { ArrowDownUpIcon } from "lucide-react";
 import { Button } from "./ui/button";
-import { DialogTrigger } from "@radix-ui/react-dialog";
 import {
   Dialog,
   DialogContent,
@@ -10,23 +9,77 @@ import {
   DialogFooter,
   DialogHeader,
   DialogTitle,
+  DialogTrigger,
 } from "./ui/dialog";
-// import { z } from "zod";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 
-// const formSchema = z.object({
-//   name: z
-//     .string()
-//     .trim()
-//     .min(2, {
-//       message: "O nome é obrigatório.",
-//     })
-//     .max(50),
-//   amount: z.string().trim().min(1, {
-//     message: "Informe um valor.",
-//   }),
-// });
+import {
+  TransactionCategory,
+  TransactionPaymentMethod,
+  TransactionType,
+} from "@prisma/client";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "./ui/form";
+import { Input } from "./ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "./ui/select";
+import { MoneyInput } from "./money-inputs";
+
+const formSchema = z.object({
+  name: z
+    .string()
+    .trim()
+    .min(2, {
+      message: "O nome é obrigatório.",
+    })
+    .max(50),
+  amount: z.string().trim().min(1, {
+    message: "Informe um valor.",
+  }),
+  type: z.nativeEnum(TransactionType, {
+    required_error: "O tipo é obrigatório.",
+  }),
+  category: z.nativeEnum(TransactionCategory, {
+    required_error: "A categoria é obrigatória.",
+  }),
+  paymentMethod: z.nativeEnum(TransactionPaymentMethod, {
+    required_error: "Por favor, informe o método de pagamento.",
+  }),
+  date: z.date({
+    required_error: "A data é obrigatória.",
+  }),
+});
 
 const AddTransactionButton = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      amount: "",
+      type: TransactionType.EXPENSE,
+      category: TransactionCategory.OTHER,
+      paymentMethod: TransactionPaymentMethod.PIX,
+      date: new Date(),
+    },
+  });
+
+  const onSubmit = (values: z.infer<typeof formSchema>) => {
+    console.log(values);
+  };
+
   return (
     <Dialog>
       <DialogTrigger asChild>
@@ -36,16 +89,80 @@ const AddTransactionButton = () => {
       </DialogTrigger>
       <DialogContent>
         <DialogHeader>
-          <DialogTitle>Are you absolutely sure?</DialogTitle>
-          <DialogDescription>
-            This action cannot be undone. This will permanently delete your
-            account and remove your data from our servers.
-          </DialogDescription>
+          <DialogTitle>Adicionar transação</DialogTitle>
+          <DialogDescription>Insira as informações abaixo</DialogDescription>
         </DialogHeader>
-        <DialogFooter>
-          <Button variant="outline">Cancelar</Button>
-          <Button>Adicionar</Button>
-        </DialogFooter>
+
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Nome</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Digite o nome..." {...field} />
+                  </FormControl>
+
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="amount"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Valor</FormLabel>
+                  <FormControl>
+                    <MoneyInput placeholder="Digite o valor..." {...field} />
+                  </FormControl>
+
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="type"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Tipo de pagamento</FormLabel>
+                  <Select
+                    onValueChange={field.onChange}
+                    defaultValue={field.value}
+                  >
+                    <FormControl>
+                      <SelectTrigger>
+                        <SelectValue placeholder="Selecione o tipo de pagamento" />
+                      </SelectTrigger>
+                    </FormControl>
+                    <SelectContent>
+                      <SelectItem value={"v@example.com"}>
+                        v@example.com
+                      </SelectItem>
+                      <SelectItem value="jj@example.com.br">
+                        jj@example.com
+                      </SelectItem>
+                      <SelectItem value="nn@example.com.br">
+                        nn@example.com
+                      </SelectItem>
+                    </SelectContent>
+                  </Select>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <DialogFooter>
+              <Button variant="outline">Cancelar</Button>
+              <Button type="submit">Adicionar</Button>
+            </DialogFooter>
+          </form>
+        </Form>
       </DialogContent>
     </Dialog>
   );
